76afdf6d0aaf64eaf857290c462119d4e08cfe31
Timaks <aksinya2016@gmail.com>
2023-09-19 14:44:48 +0300

    Раб со списками. Преобр мас-ва объ-в в мас-в эл-ов

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-  //возвращает масссив из двух объектов(значение и функция)
-  const [value, setValue] = useState('текст в инпуте');
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Post', body: 'Description'},
+    {id: 2, title: 'Post2', body: 'Description'},
+    {id: 3, title: 'Post3', body: 'Description'}
+  ]);
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'Post', body: 'Description'}}/>
-      <PostItem post={{id: 2, title: 'Post2', body: 'Description'}}/>
-      <PostItem post={{id: 3, title: 'Post3', body: 'Description'}}/>
+      <PostList posts={posts} title="Список постов 1"/>
     </div>
   );
 }
