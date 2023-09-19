c6f518934d101c8432a0cda0707931a8abc92ecc
Timaks <aksinya2016@gmail.com>
2023-09-19 11:50:48 +0300

    Props. Аргументы компонента.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
+import PostItem from "./components/PostItem";
 
 function App() {
   //возвращает масссив из двух объектов(значение и функция)
@@ -9,16 +10,9 @@ function App() {
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>Типы:</strong>
-          <div>1 тип</div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-
-      </div>
+      <PostItem post={{id: 1, title: 'Post', body: 'Description'}}/>
+      <PostItem post={{id: 2, title: 'Post2', body: 'Description'}}/>
+      <PostItem post={{id: 3, title: 'Post3', body: 'Description'}}/>
     </div>
   );
 }
